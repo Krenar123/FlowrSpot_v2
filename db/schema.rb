66587fa93d44f34928b4b1e3d5bcf0a51db56696
Krenar123 <krenardardhishta205@gmail.com>
2020-07-21 00:07:08 +0200

    Model sighting created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_162021) do
+ActiveRecord::Schema.define(version: 2020_07_20_220251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,17 @@ ActiveRecord::Schema.define(version: 2020_07_20_162021) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "sightings", force: :cascade do |t|
+    t.float "longitude"
+    t.float "latitude"
+    t.bigint "user_id", null: false
+    t.bigint "flower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flower_id"], name: "index_sightings_on_flower_id"
+    t.index ["user_id"], name: "index_sightings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
@@ -52,4 +63,6 @@ ActiveRecord::Schema.define(version: 2020_07_20_162021) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "sightings", "flowers"
+  add_foreign_key "sightings", "users"
 end
