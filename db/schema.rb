9d6a70837306ba47df1d004ea484f25509f69ee1
Krenar123 <krenardardhishta205@gmail.com>
2020-07-21 01:00:49 +0200

    Question column added to sightings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_220750) do
+ActiveRecord::Schema.define(version: 2020_07_20_221039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_07_20_220750) do
     t.bigint "flower_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "question"
     t.index ["flower_id"], name: "index_sightings_on_flower_id"
     t.index ["user_id"], name: "index_sightings_on_user_id"
   end
