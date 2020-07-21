3ab408882fc016d38a4105389a7e21d5a7184741
Krenar123 <krenardardhishta205@gmail.com>
2020-07-21 10:16:27 +0200

    Sightings controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :users,only: :create
+  resource :users, only: :create
   get '/flowers', to: 'flowers#index'
-  get '/flowers/:id', to: 'flowers#show'
+
+  resources :flowers do
+    resources :sightings, only: [:create, :index, :destroy]
+  end
+
   post '/login', to: 'users#login'
   get '/auto_login', to: 'users#auto_login'
 end
