e3209b2e0d07d0e1f9f4b70fbe8716e12a8f6cb4
Krenar Darlishta <48763668+Krenar123@users.noreply.github.com>
2024-10-25 02:31:08 +0200

    UpdateQuestionJob updated to use the service created for Trivia(External API integration)

--- app/jobs/update_question_job.rb
+++ app/jobs/update_question_job.rb
@@ -1,9 +1,13 @@
 class UpdateQuestionJob < ApplicationJob
   retry_on Net::OpenTimeout, Timeout::Error, wait: 5.seconds 
 
-  def perform(sighting_id)
-    sight = Sighting.find(sighting_id)
-    quest = JSON.parse(Net::HTTP.get(URI('https://opentdb.com/api.php?amount=1&category=18&difficulty=medium')))['results'][0]['question']
-    sight.update_attribute(:question, quest)
+  def perform!(sighting)
+    question = ThirdParty::TriviaApiService.fetch_question
+
+    sighting.update(question: question) if question.present?
+  rescue Net::OpenTimeout, Timeout::Error => e
+    Rails.logger.error("Network error occured while fetching trivia question: #{e.message}")
+  rescue StandardError => e
+    Rails.logger.error("Failed to update sighting: #{sighting.id}: #{e.message}")
   end
 end
